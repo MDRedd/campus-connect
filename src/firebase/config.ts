c9749a45c181a5d9f854dfc06d6bb682c394e1ac
M Drona Reddy <dronareddy2005@gmail.com>
2026-01-28 15:16:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4808649347-467fd",
+  "appId": "1:889642050134:web:668c68abee773a68bfd1f7",
+  "apiKey": "AIzaSyBhyxlxi8Q0EtQZSVFKhN52mwEc1h3A4hY",
+  "authDomain": "studio-4808649347-467fd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "889642050134"
+};
